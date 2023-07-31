eb74913b26b8b11c975164b23ba696cf3515bc4e
NattyNgrn <natnrgn@gmail.com>
2023-07-31 17:05:34 -0400

    knowledge check done

---
+++ domKC.js
@@ -0,0 +1,5 @@
+let bg = prompt("What background color?");
+let txtColor = prompt("What text color?")
+let body = document.querySelector("body");
+body.style.backgroundColor = bg;
+body.style.color = txtColor;
